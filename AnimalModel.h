9a52e80ad51ac6ace11cfab3855482b793577cb6
Selene <sebae97@gmail.com>
2023-02-05 21:53:34 -0500

    AnimalModel and Environmental Learning Phase
    Headers to build off of.

---
+++ AnimalModel.h
@@ -0,0 +1,79 @@
+/* Senior Project 2023 Section 1?
+*  Animal Model that can learn from data
+*/
+#pragma once
+
+// Imports
+#import "ELP.h"
+
+enum ModelOutputs
+{
+	northMag,
+	southMag,
+	westMag,
+	eastMag,
+	split,
+	merge,
+	die,
+
+	Outputs // = 7
+};
+
+enum CellAction
+{
+	act_split,
+	act_merge,
+	act_die,
+	act_nothing,
+};
+
+struct node
+{
+	double weight;
+	double bias;
+};
+
+class AnimalModel
+{
+private:
+	double* inputs;
+	int inputSize;
+	double* outputs; // Size = Outputs
+	int range; // Range of "sight" for model
+	node** hNodes; // Hidden nodes, two dimensional array of nodes
+
+public:
+	AnimalModel(int inSize, int r);
+
+	void CopyInputs(double newIn);
+
+	void Process();
+
+	int GetRange();
+
+	int GetIntSize();
+
+	double* GetOutputs();
+};
+
+class AnimalCell
+{
+private:
+	AnimalModel* model;
+	double lon;
+	double lat;
+	double horVector; // Horizontal vector, + for East and - for West
+	double verVector; // Vertical vector, + for North and - for South
+	CellAction next; // next action
+
+public:
+	AnimalCell(AnimalModel m, double xCoord, double yCoord);
+
+	void Observe(Grid* grid); // Observes nearby squares using coordinates and grid range to inputs
+
+	AnimalCell* Split();
+
+	void Merge(AnimalCell* partner); // may be programmed to either be a "Consumptive" merge where one cell "eats" the other, or a mutual merge where they must both agree to merge
+
+	void Die();
+};
---
+++ ELP.h
@@ -0,0 +1,12 @@
+/* Senior Project 2023 Section 1?
+*  Environmental Learning Phase format
+*/
+#pragma once
+
+class Grid
+{
+	char* name;
+	double** grid;
+	int xSize;
+	int ySize;
+};
